014d8786a5cd706cf0a7fb5a08f7b61e02527078
vanya <vanchello089@gmail.com>
2022-09-22 16:19:42 +0300

    add db

---
+++ server/models/models.js
@@ -0,0 +1,15 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: "USER"}
+})
+
+const Basket = sequelize.define('basket', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    user: {type: DataTypes.STRING, unique: true},
+
+})
