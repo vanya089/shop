3cc58a3a81a5014752b80fafe88f9a26ef325889
vanya <vanchello089@gmail.com>
2022-10-16 19:35:44 +0500

    todo_server: add crud

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,4 @@
+const ApiError = require('../error/ApiError')
 class UserController{
     async registration(req,res){
 
@@ -5,8 +6,12 @@ class UserController{
     async login(req,res){
 
     }
-    async check(req,res){
-        res.json('asdasdasdasdasd')
+    async check(req, res, next){
+        const {id} = req.query
+        if (!id) {
+            return next(ApiError.badRequest('Не задан id!'))
+        }
+        res.json(id)
     }
 }
 
--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
+const fileUpload = require('express-fileupload')
 const cors = require('cors')
 const router = require('./routes/index')
+const errorHandler = require('./midleware/ErrorHandlerMidleware')
+const path = require('path')
 
 
 const PORT = process.env.PORT || 5000
@@ -11,7 +14,10 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 app.use('/api', router)
+app.use(errorHandler)
 
 
 app.get('/', (req,res) => {
