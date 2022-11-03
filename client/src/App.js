dca39bbca3886a4a096c27bdadfd53bd85c187f6
vanya <vanchello089@gmail.com>
2022-11-03 13:08:13 +0500

    todo_client: add mobx and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
+import AppRouter from "./components/AppRouter";
+import {BrowserRouter} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      work
-    </div>
+    <BrowserRouter>
+      <AppRouter/>
+    </BrowserRouter>
   );
 }
 
